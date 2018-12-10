334d0a7674763c713e507f1daaf45c2bfc1d387f
Orkun Kulce <kulceorkun@gmail.com>
2018-12-10 15:09:45 +0300

    Add nats client

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/nats-io/go-nats"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -8,6 +9,8 @@ import (
 
 func main() {
 	e := echo.New()
+	nc, _ := nats.Connect(nats.DefaultURL)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
